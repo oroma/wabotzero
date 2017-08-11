ae695dd0197705f63b766a0663ad0e0aad9306fa
Sanghoon <noohgnas@gmail.com>
2017-08-11 21:15:05 +0900

    Update mega.ino

--- mega.ino
+++ mega.ino
@@ -1,6 +1,10 @@
 #include <SoftwareSerial.h>
-
+#include <Servo.h>  //add '<' and '>' before and after servo.h
+ 
+int joint_l_0_pin = 4; // pwm pin 4
+ 
 SoftwareSerial inSerial(2,3);
+Servo joint_l_0;
 
 #define MAX_CMD_COUNT 7
 
@@ -8,6 +12,9 @@ void setup() {
   Serial1.begin(9600);
   Serial.begin(9600);
   inSerial.begin(9600);
+
+//  pinMode(4, OUTPUT);
+  joint_l_0.attach(joint_l_0_pin);
 }
 
 int doMovingByPreset(unsigned char preset)
@@ -27,6 +34,8 @@ int doMovingJointArm(char lr, unsigned char j0, unsigned char j1, unsigned char 
     Serial.print("right, ");
   }
   Serial.print(j0, DEC);
+  joint_l_0.write((int)j0);      // Turn SG90 servo back to 90 degrees (center position)
+  delay(2000);
   Serial.print(' ');
   Serial.print(j1, DEC);
   Serial.print(' ');
@@ -51,31 +60,33 @@ int doMovingWheel(unsigned char d, unsigned char m)
 char testCmds[MAX_CMD_COUNT][10] = {
   { 'P', 0x01, '\n' },
   { 'P', 0x02, '\n' },
-  { 'M', 'J', 'A', 'L', 0xB4, 0xB4, 0xB4,'\n' },
-  { 'M', 'J', 'A', 'R', 0xB4, 0xB4, 0xB4,'\n' },
+  { 'M', 'J', 'A', 'L', 20, 0xB4, 0xB4,'\n' },
+  { 'M', 'J', 'A', 'R', 50, 0xB4, 0xB4,'\n' },
   { 'M', 'J', 'D', 0xB4, '\n' },
   { 'M', 'W', 'D', 'F', '\n' },
   { 'M', 'W', 'D', 'B', '\n' }
 };
 
 void loop() {
-  for (int i; i < MAX_CMD_COUNT; i++) {
-    if (testCmds[i][0] == 'M') {
-      if (testCmds[i][1] == 'J' && testCmds[i][2] == 'A')
-        doMovingJointArm(testCmds[i][3], testCmds[i][4], testCmds[i][5], testCmds[i][6]);
-      if (testCmds[i][1] == 'J' && testCmds[i][2] == 'D')
-        doMovingJointDisc(testCmds[i][3]);
-      if (testCmds[i][1] == 'W' && testCmds[i][2] == 'D')
-        doMovingWheel(testCmds[i][3], testCmds[i][4]);
-    } else if (testCmds[i][0] == 'P') {
-      doMovingByPreset(testCmds[i][1]);
+  while (1) {
+    for (int i; i < MAX_CMD_COUNT; i++) {
+      if (testCmds[i][0] == 'M') {
+        if (testCmds[i][1] == 'J' && testCmds[i][2] == 'A')
+          doMovingJointArm(testCmds[i][3], testCmds[i][4], testCmds[i][5], testCmds[i][6]);
+        if (testCmds[i][1] == 'J' && testCmds[i][2] == 'D')
+          doMovingJointDisc(testCmds[i][3]);
+        if (testCmds[i][1] == 'W' && testCmds[i][2] == 'D')
+          doMovingWheel(testCmds[i][3], testCmds[i][4]);
+      } else if (testCmds[i][0] == 'P') {
+        doMovingByPreset(testCmds[i][1]);
+      }
+      Serial.println();
     }
 
-    Serial.println();
   }
-  while (Serial1.available()) {
-   
-      Serial.print(Serial1.read(), HEX);
-      Serial.println();
-  }
+//  while (Serial1.available()) {
+//   
+//      Serial.print(Serial1.read(), HEX);
+//      Serial.println();
+//  }
 }
