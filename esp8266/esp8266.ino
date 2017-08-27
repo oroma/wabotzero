dd9d32b4c25c2e52aff0b0e9f109eced761920f9
Sanghoon <noohgnas@gmail.com>
2017-08-27 10:11:46 +0900

    명령어 생성, mega 시리얼 전송

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -8,11 +8,16 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <SoftwareSerial.h>
 
 const char *ssid = "arduino-er";
 const char *password = "password";
 
 ESP8266WebServer server(80);
+
+const byte rxPin = 5;
+const byte txPin = 6;
+SoftwareSerial toMega(rxPin, txPin);
 
 void handleRoot() {
   Serial.println(server.uri());
@@ -35,7 +40,51 @@ void handleJointCommand() {
   Serial.println(server.method());
   Serial.println(server.args());
 
-  response();
+  // range: 30 ~ 150
+  int errorCode = 200;
+  char cmd[8] = { 'M', 'J', 'A', 'L', 0xff, 0xff, 0xff, '\0' };
+
+  // e.g.) http://192.168.4.1/command/joint?part=a&d=l&no=2&angle=110
+  if (server.hasArg("part")  && 
+      server.hasArg("d")     &&
+      server.hasArg("no")    &&
+      server.hasArg("angle"))
+  {
+    cmd[2] = toupper(server.arg("part")[0]);  // part
+    cmd[3] = toupper(server.arg("d")[0]);  // direction
+    cmd[4 + server.arg("no").toInt()] = (unsigned char)server.arg("angle").toInt();
+  } else {
+    errorCode = 400; // Bad Request
+  }
+  Serial.println(cmd);
+
+  // send to mega
+  int res = toMega.write(cmd);
+  Serial.print(res, DEC);
+
+  String r = "" + res;
+  String message = "";
+  message += "{ \"uri\": ";
+  message += "\"" + server.uri() + "\",";
+  message += "\"method\": \"";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\",";
+  message += "\"command\": \"";
+  message += cmd;
+  message += "\",";
+  message += "\"written\": \"";
+  message += "" + res;
+  message += "\",";
+  message += "\"arguments\": [";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " { \"" + server.argName ( i ) + "\": \"" + server.arg ( i ) + "\" }";
+    if (i < server.args() - 1)
+      message += ", ";
+  }
+  message += "]}";
+
+  server.send(errorCode, "application/json", message);
 }
 
 void handleWheelCommand() {
